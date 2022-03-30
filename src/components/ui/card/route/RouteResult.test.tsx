3de508fdb6406a5e8e49edac5bff6e15fcb6bc50
EriRan <erik.rantanen@hotmail.fi>
2022-03-30 18:44:10 +0300

    Add simple snapshot test
    Ok interesting

--- src/components/ui/card/route/RouteResult.test.tsx
+++ src/components/ui/card/route/RouteResult.test.tsx
@@ -1,13 +1,22 @@
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
+import { Store, AnyAction } from 'redux';
 import { createRouteLookupStore } from '../../../../reducers';
+import { RootState } from '../../../../reducers/types';
 import RouteResult from './RouteResult';
 
-test('Render it', () => {
+let store: Store<RootState, AnyAction>; 
+
+beforeEach(() => {
+  store = createRouteLookupStore();
+});
+
+test('Renders empty div when no content', () => {
+  store.getState().route.calculatedRoute = null;
   // TODO: Do something about i18n translation warning here
   const component = renderer.create(
-    <Provider store={createRouteLookupStore()}><RouteResult /></Provider>,
+    <Provider store={store}><RouteResult /></Provider>,
   );
 
-  expect(component).toBeDefined();
+  expect(component).toMatchSnapshot();
 });
