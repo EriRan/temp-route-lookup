5b5259e74043a55e1ab39d14937a37dc15cdace1
EriRan <erik.rantanen@hotmail.fi>
2023-01-02 11:08:14 +0200

    Add ThemeProvider wrapper
    Needed in Material UI 5 update

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,22 @@ import "./i18n";
 import App from "./components/App";
 import { createRouteLookupStore } from "./reducers";
 import { createRoot } from "react-dom/client";
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
+const theme = createMuiTheme();
+
+// Wrap the application with Redux state management and Material UI theme
 root.render(
   <Provider store={createRouteLookupStore()}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>
 );
