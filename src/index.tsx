0f3508acf38972f8d3a2217d954b5fbc8358aebb
EriRan <erik.rantanen@hotmail.fi>
2023-01-02 11:45:32 +0200

    Revert ThemeProvider usage
    Material UI 5 does not work with React 18 so not upgrading it just yet

--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,12 @@ import "./i18n";
 import App from "./components/App";
 import { createRouteLookupStore } from "./reducers";
 import { createRoot } from "react-dom/client";
-import {
-  createMuiTheme,
-  makeStyles,
-  ThemeProvider,
-} from "@material-ui/core/styles";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
-const theme = createMuiTheme();
-
-// Wrap the application with Redux state management and Material UI theme
 root.render(
   <Provider store={createRouteLookupStore()}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </Provider>
 );
