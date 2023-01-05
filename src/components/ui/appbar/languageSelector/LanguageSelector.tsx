030154243d94e58aea224e3adac7fb82aa6a5046
EriRan <erik.rantanen@hotmail.fi>
2023-01-05 15:33:54 +0200

    Add TSDoc

--- src/components/ui/appbar/languageSelector/LanguageSelector.tsx
+++ src/components/ui/appbar/languageSelector/LanguageSelector.tsx
@@ -3,13 +3,24 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import i18n from "../../../../i18n";
 import { RootState } from "../../../../reducers";
-import { closeLanguageDropdown, openLanguageDropdown } from "../../../../reducers/language/languageReducer";
+import {
+  closeLanguageDropdown,
+  openLanguageDropdown,
+} from "../../../../reducers/language/languageReducer";
 import LanguageSelectorItem from "./languageSelectorItem/LanguageSelectorItem";
 import convertLanguageFlagEmoji from "./languageToFlagEmojiConverter";
 
+/**
+ * Component that displays selectable languages when opened.
+ *
+ * Has the HTMLElement it is anchored to in its own state
+ * @returns LanguageSelector component
+ */
 export default function LanguageSelector() {
   const dispatch = useDispatch();
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
   const languageState = useSelector((state: RootState) => {
     return {
       isLanguageDropdownOpen: state.language.isLanguageDropdownOpen,
