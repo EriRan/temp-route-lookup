36813f8143c46941b5b0cd28a8774fd1557c9a36
EriRan <erik.rantanen@hotmail.fi>
2023-01-10 15:39:23 +0200

    Add test
    The test does not work if I don't use stop names from the original
    transportData.json. Need to move it to state one day so that I can add
    values to it. This is really good experience to encounter these problems
    and in future mock them so that testing is easier

--- src/components/ui/card/route/form/input/RouteInput.test.tsx
+++ src/components/ui/card/route/form/input/RouteInput.test.tsx
@@ -26,7 +26,7 @@ describe("RouteInput", () => {
           route: {
             startStop: inputStopData,
             destinationStop: {
-              name: null
+              name: null,
             },
             calculatedRoute: {
               totalDuration: null,
@@ -164,8 +164,10 @@ describe("RouteInput", () => {
   });
 
   test("Start stop: State changes when typing done to input", async () => {
-    const initialStopName = "initialStop";
-    const typedStopName = "typedStop";
+    // TODO: The fact that transportation data is always coming from the JSON really limits my choices of names here
+    // Validator will add an error to the start stop if the start stop name does not match. Really need to move the transport data into state to resolve this
+    const initialStopName = "A";
+    const typedStopName = "B";
     const stopMap = new Map();
     const initialStop: Stop = {
       name: initialStopName,
@@ -206,6 +208,49 @@ describe("RouteInput", () => {
     await user.clear(castedInputTextField);
     await user.type(castedInputTextField, typedStopName);
 
+    expect(castedInputTextField).toBeValid();
     expect(castedInputTextField).toHaveValue(typedStopName);
+  });
+
+  test("Type to non existant value displays an error", async () => {
+    const initialStopName = "initialStop";
+    const nonExistantStopName = "nonExistantStop";
+    const stopMap = new Map();
+    const initialStop: Stop = {
+      name: initialStopName,
+      roads: [],
+    };
+    const inputStopData: StopState = {
+      name: initialStopName,
+    };
+    stopMap.set(initialStop.name, initialStop);
+
+    const user = userEvent.setup();
+    renderWithProviders(
+      <RouteInput type={RouteInputType.START} stopMap={stopMap} />,
+      {
+        preloadedState: {
+          route: {
+            startStop: inputStopData,
+            destinationStop: {
+              name: null,
+            },
+            calculatedRoute: {
+              totalDuration: null,
+              route: [],
+              errorMessages: [],
+            },
+          },
+        },
+      }
+    );
+    const inputTextField = screen.getByRole("textbox");
+    expect(inputTextField instanceof HTMLInputElement).toBeTruthy();
+    const castedInputTextField = inputTextField as HTMLInputElement;
+    await user.clear(castedInputTextField);
+    await user.type(castedInputTextField, nonExistantStopName);
+
+    expect(castedInputTextField).toHaveValue(nonExistantStopName);
+    expect(castedInputTextField).toBeInvalid();
   });
 });
