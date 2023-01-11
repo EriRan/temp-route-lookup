409ac60a8b0e68cd368c7bcbc0f164a2e258a9b1
EriRan <erik.rantanen@hotmail.fi>
2023-01-11 09:09:47 +0200

    Combine files
    Its still less than 100 rows after combining so this is fine

--- src/components/ui/card/RouteCard.tsx
+++ src/components/ui/card/RouteCard.tsx
@@ -1,11 +1,13 @@
 import { FunctionComponent } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { Card } from "@material-ui/core";
-import RouteCardContent from "./RouteCardContent";
+import { Accordion, AccordionDetails, Card, Divider, Grid } from "@material-ui/core";
 import { UiContainerProps } from "../types";
+import RouteCardHeader from "./header/RouteCardHeader";
+import RouteForm from "./route/form/RouteForm";
+import RouteResult from "./route/result/RouteResult";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     position: "fixed",
     zIndex: 1,
@@ -20,7 +22,17 @@ const RouteCard: FunctionComponent<UiContainerProps> = ({ transportData }) => {
   const classes = useStyles();
   return (
     <Card className={classes.root} raised={true}>
-      <RouteCardContent transportData={transportData} />
+      <Accordion>
+        <RouteCardHeader />
+        <Divider />
+        <AccordionDetails>
+          <Grid container direction="column">
+            <RouteForm stopMap={transportData.stopMap} />
+            <Divider />
+            <RouteResult />
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
     </Card>
   );
 };
