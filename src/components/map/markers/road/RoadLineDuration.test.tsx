fcc4492b047f75b31e978060634bee360623c383
EriRan <erik.rantanen@hotmail.fi>
2023-01-12 13:10:23 +0200

    Add test

---
+++ src/components/map/markers/road/RoadLineDuration.test.tsx
@@ -0,0 +1,109 @@
+import { renderWithProviders } from "test-utils";
+import { screen } from "@testing-library/react";
+import RoadLineDuration from "./RoadLineDuration";
+
+describe("RoadLineDuration", () => {
+  test("No duration if no startpoint or endpoint", () => {
+    renderWithProviders(<RoadLineDuration duration={10} />);
+
+    expect(screen.queryByText("10")).not.toBeInTheDocument();
+  });
+
+  test("No duration if no endpoint", () => {
+    renderWithProviders(
+      <svg>
+        <RoadLineDuration
+          duration={10}
+          startPointLocation={{
+            x: 0,
+            y: 10,
+          }}
+        />
+      </svg>
+    );
+
+    expect(screen.queryByText("10")).not.toBeInTheDocument();
+  });
+
+  test("Duration available if both start and endpoint set", () => {
+    renderWithProviders(
+      <svg>
+        <RoadLineDuration
+          duration={10}
+          startPointLocation={{
+            x: 0,
+            y: 10,
+          }}
+          endPointLocation={{
+            x: 0,
+            y: 20,
+          }}
+        />
+      </svg>
+    );
+    expect(screen.getByText("10")).toBeInTheDocument();
+  });
+
+  test("Horizontal midpoint", () => {
+    const renderResult = renderWithProviders(
+      <svg>
+        <RoadLineDuration
+          duration={10}
+          startPointLocation={{
+            x: 0,
+            y: 0,
+          }}
+          endPointLocation={{
+            x: 0,
+            y: 50,
+          }}
+        />
+      </svg>
+    );
+    // I wonder if there is a better way to test this? Could not find a way where I could apply Typescript into the HTML elements and then use x and y
+    expect(renderResult.container.querySelector("[x='0']")).toBeDefined();
+    expect(renderResult.container.querySelector("[y='30']")).toBeDefined();
+  });
+
+  test("Vertical midpoint", () => {
+    const renderResult = renderWithProviders(
+      <svg>
+        <RoadLineDuration
+          duration={10}
+          startPointLocation={{
+            x: 0,
+            y: 0,
+          }}
+          endPointLocation={{
+            x: 50,
+            y: 0,
+          }}
+        />
+      </svg>
+    );
+    // I wonder if there is a better way to test this? Could not find a way where I could apply Typescript into the HTML elements and then use x and y
+    expect(renderResult.container.querySelector("[x='30']")).toBeDefined();
+    expect(renderResult.container.querySelector("[y='0']")).toBeDefined();
+  });
+
+  test("Diagonal midpoint", () => {
+    const renderResult = renderWithProviders(
+      <svg>
+        <RoadLineDuration
+          duration={10}
+          startPointLocation={{
+            x: 0,
+            y: 0,
+          }}
+          endPointLocation={{
+            x: 50,
+            y: 50,
+          }}
+        />
+      </svg>
+    );
+    // I wonder if there is a better way to test this? Could not find a way where I could apply Typescript into the HTML elements and then use x and y
+    expect(renderResult.container.querySelector("[x='25']")).toBeDefined();
+    expect(renderResult.container.querySelector("[y='30']")).toBeDefined();
+  });
+});
