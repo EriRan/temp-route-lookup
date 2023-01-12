f450fc6409d5f32e207d1066d378c6cd0a9d2a1a
EriRan <erik.rantanen@hotmail.fi>
2023-01-12 10:06:56 +0200

    Use function to get node environment
    Easier to mock this

---
+++ src/components/constant/EnvironmentVariable.ts
@@ -0,0 +1,3 @@
+export function getNodeEnvironment() {
+  return process.env.NODE_ENV;
+}
--- src/components/ui/appbar/KuutiolaAppBarSubtitle.tsx
+++ src/components/ui/appbar/KuutiolaAppBarSubtitle.tsx
@@ -3,16 +3,16 @@ import { FunctionComponent } from "react";
 import { Typography } from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import { LOCAL_DEVELOPMENT_VERSION } from "../../constant/TranslationKeyConstant";
+import { getNodeEnvironment } from "../../constant/EnvironmentVariable";
 
 /**
  * Secondary title that is displayed if using a development build
  */
 const KuutiolaAppBarSubtitle: FunctionComponent<{}> = () => {
   const { t } = useTranslation();
-
   return (
     <Typography variant="h6" align="center" color="secondary">
-      {process.env.NODE_ENV === "development"
+      {getNodeEnvironment() === "development"
         ? t(LOCAL_DEVELOPMENT_VERSION)
         : ""}
     </Typography>
