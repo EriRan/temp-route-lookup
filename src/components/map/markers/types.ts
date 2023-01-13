4d380febb4d85d84b565a1d661f7a1b79622504e
EriRan <erik.rantanen@hotmail.fi>
2023-01-13 13:49:15 +0200

    Add missing required type
    Always added in MapView

--- src/components/map/markers/types.ts
+++ src/components/map/markers/types.ts
@@ -1,5 +1,7 @@
+import { Stop } from "../../../data/mapper/types";
 import { BusStopLocation, MapProps } from "../types";
 
 export interface BusTrafficContainerProps extends MapProps {
   busStopLocationMap: Map<string, BusStopLocation>;
+  stopMap: Map<string, Stop>
 }
