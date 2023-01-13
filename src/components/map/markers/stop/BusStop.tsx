11445651c6972abc4b515ff66dced99d1550bd93
EriRan <erik.rantanen@hotmail.fi>
2023-01-13 12:12:46 +0200

    Finish migration into functional component
    More clear this way

--- src/components/map/markers/stop/BusStop.tsx
+++ src/components/map/markers/stop/BusStop.tsx
@@ -1,6 +1,4 @@
 import { FunctionComponent } from "react";
-import { connect, ConnectedProps } from "react-redux";
-
 import {
   BUS_STOP_CIRCLE_RADIUS,
   SELECTED_STOP_COLOR,
@@ -9,23 +7,66 @@ import {
 import "./BusStop.css";
 import { Payload, StopState } from "../../../../reducers/route/types";
 import { createStyles, makeStyles, Theme } from "@material-ui/core";
-import { RootState } from "../../../../reducers";
 import { stopClicked } from "../../../../reducers/route/routeReducer";
 import { ActionCreatorWithPayload } from "@reduxjs/toolkit";
+import { useAppDispatch, useAppSelector } from "../../../../reducers/hooks";
 
-const BusStop: FunctionComponent<Props> = (props) => {
+const BusStop: FunctionComponent<BusStopProps> = (props) => {
+  const dispatch = useAppDispatch();
+  const routeState = useAppSelector((state) => {
+    return {
+      startStop: state.route.startStop,
+      destinationStop: state.route.destinationStop,
+    };
+  });
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        ...theme.typography.button,
+      },
+    })
+  );
+
+  const deduceStrokeColor = (
+    currentStopName: string,
+    startStop: StopState | null,
+    destinationStop: StopState | null
+  ) => {
+    if (
+      (startStop && currentStopName === startStop.name?.toUpperCase()) ||
+      (destinationStop &&
+        currentStopName === destinationStop.name?.toUpperCase())
+    ) {
+      return SELECTED_STOP_COLOR;
+    }
+    return UNSELECTED_STOP_COLOR;
+  };
+
+  const handleClick = (
+    name: string,
+    stopClicked: ActionCreatorWithPayload<Payload, string>
+  ) => {
+    dispatch({
+      type: stopClicked.type,
+      payload: {
+        name: name,
+      },
+    });
+  };
+
   //Create style class with font from Material UI. We want the default button text style from here
   const classes = useStyles();
+
   return (
-    <g onClick={() => handleClick(props.name, props.stopClicked)}>
+    <g onClick={() => handleClick(props.name, stopClicked)}>
       <circle
         cx={props.x}
         cy={props.y}
         r={BUS_STOP_CIRCLE_RADIUS.toString()}
         stroke={deduceStrokeColor(
           props.name,
-          props.startStop,
-          props.destinationStop
+          routeState.startStop,
+          routeState.destinationStop
         )}
       ></circle>
       <text x={props.x} y={props.y + 5} className={classes.root}>
@@ -35,51 +76,10 @@ const BusStop: FunctionComponent<Props> = (props) => {
   );
 };
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      ...theme.typography.button,
-    },
-  })
-);
-
-const deduceStrokeColor = (
-  currentStopName: string,
-  startStop: StopState | null,
-  destinationStop: StopState | null
-) => {
-  if (
-    (startStop && currentStopName === startStop.name?.toUpperCase()) ||
-    (destinationStop && currentStopName === destinationStop.name?.toUpperCase())
-  ) {
-    return SELECTED_STOP_COLOR;
-  }
-  return UNSELECTED_STOP_COLOR;
-};
-
-const handleClick = (
-  name: string,
-  stopClicked: ActionCreatorWithPayload<Payload, string>
-) => {
-  stopClicked({ name: name });
-};
-
-//Let's create a Typescript type from redux props and the props that are provided to this bus stop
-type PropsFromRedux = ConnectedProps<typeof connector>;
-
-type Props = PropsFromRedux & {
+type BusStopProps = {
   name: string;
   x: number;
   y: number;
 };
 
-const mapStateToProps = (state: RootState) => {
-  return {
-    startStop: state.route.startStop,
-    destinationStop: state.route.destinationStop,
-  };
-};
-
-const connector = connect(mapStateToProps, { stopClicked });
-
-export default connector(BusStop);
+export default BusStop;
