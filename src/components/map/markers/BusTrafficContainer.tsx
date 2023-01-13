5eb5b9ba47c893fb3601bd009655f939072b3d70
EriRan <erik.rantanen@hotmail.fi>
2023-01-13 13:30:32 +0200

    Migrate to functional component

--- src/components/map/markers/BusTrafficContainer.tsx
+++ src/components/map/markers/BusTrafficContainer.tsx
@@ -1,38 +1,31 @@
-import React from "react";
+import { FunctionComponent } from "react";
 
 import BusStop from "./stop/BusStop";
 import RoadContainer from "./road/RoadContainer";
 import { BusTrafficContainerProps } from "./types";
 import { BusStopLocation } from "../types";
 
-class BusTrafficContainer extends React.Component<
-  BusTrafficContainerProps,
-  {}
-> {
-  render() {
-    return (
-      <g className="bus-traffic-container">{this.renderTrafficNetwork()}</g>
-    );
-  }
-
-  renderTrafficNetwork() {
-    if (!this.props.busStopLocationMap) {
+const BusTrafficContainer: FunctionComponent<BusTrafficContainerProps> = (
+  props
+) => {
+  const renderTrafficNetwork = () => {
+    if (!props.busStopLocationMap) {
       return "Loading bus stops...";
     }
     return (
       <g className="bus-traffic-container">
         <RoadContainer
-          busStopLocationMap={this.props.busStopLocationMap}
-          roads={Array.from(this.props.stopMap.values()).flatMap(
+          busStopLocationMap={props.busStopLocationMap}
+          roads={Array.from(props.stopMap.values()).flatMap(
             (stop) => stop.roads
           )}
         />
-        {this.renderBusStops(this.props.busStopLocationMap)}
+        {renderBusStops(props.busStopLocationMap)}
       </g>
     );
-  }
+  };
 
-  private renderBusStops(busStopLocationMap: Map<string, BusStopLocation>) {
+  const renderBusStops = (busStopLocationMap: Map<string, BusStopLocation>) => {
     return (
       <g className="bus-stop-container">
         {Array.from(busStopLocationMap.entries()).map(
@@ -49,7 +42,9 @@ class BusTrafficContainer extends React.Component<
         )}
       </g>
     );
-  }
-}
+  };
+
+  return <g className="bus-traffic-container">{renderTrafficNetwork()}</g>;
+};
 
 export default BusTrafficContainer;
